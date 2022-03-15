1b61e1f24e5d7fa6affe917c579a0f6e5b4668ae
Timo Noko <timonoko@users.noreply.github.com>
2022-03-15 20:41:08 +0200

    For the record

---
+++ tmp.py
@@ -0,0 +1,24 @@
+
+
+def gensym1(AB=[]):
+    if equal(AB,"A"):
+        XO=0
+        OX=AOX
+        OY=AOY
+    elif True:
+        XO=900
+        OX=BOX
+        OY=BOY
+    paska=0.35
+    kaks=700
+    for x in range(-449,(1 + 449)):
+        for y in range(-449,(1 + 449)):
+            uusi=(paska + (sqrt(((x * x) + (y * y))) / kaks))
+            AR2=(AR / uusi)
+            putpixel(IM,int((x + (451 + XO))),int((y + 451)),getpixel(EKA,int((OX + (AR2 * (x / 450)))),int((OY + (AR2 * (y / 450))))))
+    return IM
+
+simple_copy=gensym1
+defq('simple_copy','lambda x: simple_copy(a1(x))')
+
+
